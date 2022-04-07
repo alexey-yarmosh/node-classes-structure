0e1e0720b94ec78555d3fc5b4a60fc9419c53bae
aliaksei-yarmash <delete-863@yandex.by>
2022-04-07 12:36:34 +0300

    fix json issue

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
+# Node.js built-in classes inheritance structure
+
+https://yarmosh.by/node-classes-structure
+
 ## TODO
 
-- add classes that are only created by fabrics, not constructors
-- add descriptions
-- add node vesion, design, title, and global description
-- host it
-
 Additional classes in urls:
+
 https://nodejs.org/api/async_hooks.html#class-asynchook
+
 https://nodejs.org/api/events.html
+
 https://nodejs.org/api/globals.html
+
 https://nodejs.org/api/crypto.html
+
 https://nodejs.org/api/zlib.html
+
 https://nodejs.org/api/http2.html
+
 https://nodejs.org/api/tls.html
+
+...
+
 module.builtinModules
-... many others
--- static/chart.js
+++ static/chart.js
@@ -1,12 +1,16 @@
-fetch("../data/data-v2-with-comments.json")
+fetch("data/data-with-comments.json")
 .then(response => {
   return response.json();
 })
 .then(jsondata => drawChart(jsondata));
 
 function drawChart(data) {
-  JSC.chart('chartDiv', {
+  const ch = JSC.chart('chartDiv', {
     type: 'organization right',
+    defaultTooltip: {
+      outline: 'none',
+      asHTML: true
+    },
     legend_visible: false,
     series: [
       {
@@ -14,16 +18,20 @@ function drawChart(data) {
         defaultPoint: {
           label: {
             text: '%name',
-            autoWrap: false
+            autoWrap: false,
+            style: {
+              fontSize: 10
+            }
           },
           annotation: {
             padding: 9,
             corners: ['cut', 'square', 'cut', 'square'],
-            margin: [15, 5, 10, 0]
+            margin: [10, 0, 10, 0]
           },
           outline: { color: '#73bc6e', width: 1 },
           color: '#dcead7',
-          tooltip: '<b>%name</b><br/>%description'
+          // tooltip: '<b>%name</b><br/>%description'
+          tooltip: '<div class="tooltip-box"><b>%name</b><br/>%description</div>'
         },
         points: data
       }
